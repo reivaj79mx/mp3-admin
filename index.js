84bdbf2712961644c5e37d7a946dbd6f32ee6817
Javier Reta <javier.reta@inegi.org.mx>
2017-03-02 16:43:50 -0600

    Primeras aproximaciones para el comando 'scan'

--- index.js
+++ index.js
@@ -2,49 +2,39 @@
 'use strict';
 
 const program = require('commander');
-const fs = require('fs');
 const id3 = require('id3-parser');
 const _ = require('lodash');
 
-const x = require('./lib/fs-oper');
+const fops = require('./lib/fs-oper');
 
 program
   .version('0.1.0')
-  .command('list {path}')
+  .command('scan [path]')
   .description('Lista las etiquetas ID3 encontradas en el path')
-  .action(_list);
+  .action(scan);
 
 program.parse(process.argv);
 
-function _list() {
-  var directory = program.args[0];
-  x.readDirectory(directory).then(files => {
-    console.log(files);
-  });  
+function scan(path) {
+  if (!path) path = '.';
 
-  /*readDirectory(path).then(files => { 
-      _.forEach(files, file => {
-        if (_.endsWith(file, '.mp3')) {
-          readFile(`${path}/${file}`).then((data) => {
-            id3.parse(data).then(tags => {
-              console.log(tags.track, tags.title);
-            });
-          });
-        }
-      })
-    }).catch(error => console.log(error));*/
-}
-
-
-
-function readFile(file) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, (error, data) => {
-      if (error) {
-        return reject(error);
-      }
-
-      return resolve(data);
+  fops.readDirectory(path).then((files) => {
+    return _.filter(files, (file) => { 
+      return _.endsWith(file, '.mp3'); 
     });
+  }).then((files) => {
+    return Promise.all(_.map(files, (file) => {
+      return fops.readFile(file);
+    }));
+  }).then((datas) => {
+    return Promise.all(_.map(datas, (data) => {
+      return id3.parse(data);
+    }))
+  }).then((tags) => {
+    let artist = _.groupBy(tags, (tag) => {
+      return tag.artist;
+    })
+
+    console.log(x);
   })
 }
